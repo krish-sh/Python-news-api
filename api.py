e7146012c13e13af66cb2e6566ed82d3ec41e04a
Krish Sharma <krishsharma3355@gmail.com>
2026-07-05 16:53:28 +0530

    handling of response

--- api.py
+++ api.py
@@ -11,4 +11,19 @@ if not API_KEY:
 
 keyword = input("Enter a keyword to search for news articles:")
 
-url = f"https://newsapi.org/v2/everything?q={keyword}&sortBy=publishedAt&apiKey={API_KEY}"
+url = f"https://newsapi.org/v2/everything?q={keyword}&pageSize=5&apiKey={API_KEY}"
+request = requests.get(url)
+
+if request.status_code == 200:
+    data = request.json()
+    articles = data.get("articles")
+    print(data)
+    print(f"Top Headlines")
+    for i, articel in enumerate(articles, start=1):
+        print(f"{i}: Author: {articel["author"]}\n Title: {articel["title"]}")
+        print(f"\nDescription: {articel["description"]}")
+        print(f"\nPublishedAt: {articel["publishedAt"]}")
+        print(f"\nContent: {articel["content"]}")
+        print("-" * 60)
+else:
+    print("request is not going through the Url")
